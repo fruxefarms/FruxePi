aa26daf94995e6c4cbe7ad60f99b424a9bdff835
Jay Yeo <jay@fruxe.co>
2019-04-02 22:26:17 +0200

    Install script UI colours and formatting

--- install.sh
+++ install.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
 # FRUXEPI DOCKER installation
-echo 'Starting FruxePi Installation'
+echo ""
+echo -e "\e[1m\e[45mFruxePi v0.2-BETA Install\e[0m"
+echo -e "\e[1mversion:\e[0m \e[37mfrx-pi-v0.2-BETA\e[0m"
+echo -e "\e[1mweb:\e[0m \e[37mdocs.fruxe.co\e[0m"
+echo ""
 
 # To get the latest package lists
-echo 'Getting Latest Updates...'
+echo -e "\e[35mGetting Latest Updates...\e[0m"
 {
    sudo apt-get update
 } &> /dev/null
 
 # Install required packages
-echo 'Installing required packages..'
+echo -e "\e[35mInstalling required packages..\e[0m"
 {
 sudo apt-get install -y python python-pip
 } &> /dev/null
@@ -37,33 +41,37 @@ if [ ! -x "$(command -v docker)" ]; then
 fi
 
 # Create FruxePi Docker containers
-echo 'Building Docker containers... This will take several minutes.'
-
+echo -e "\e[35mBuilding Docker containers...\e[0m \e[5mThis will take several minutes.\e[0m" 
+{
 cd docker/
 sudo docker-compose up -d
+} &> /dev/null
 
 # Check if containers were created
-echo 'Configuring Docker containers...'
+echo -e "\e[35mConfiguring Docker containers...\e[0m"
 if [ "$(docker ps -q -f name=frxpi-APACHE)" ] && [ "$(docker ps -q -f name=frxpi-MYSQL)" ]; then
+   {
    sudo docker exec -it frxpi-APACHE /bin/bash -c 'chmod +x /var/www/html/actions/fruxepi.py;'
    sudo docker exec -it frxpi-APACHE /bin/bash -c 'chmod 777 /var/www/html/assets/tmp/crontab.txt;'
    sudo docker exec -it frxpi-MYSQL /bin/bash -c 'mysql -u root -pfruxefarms frx_db < /docker-entrypoint-initdb.d/frx_db.sql;'
    sudo docker exec -it frxpi-APACHE /bin/bash -c 'bash /tmp/cron_init.sh;'
+   } &> /dev/null
 else
-   echo "Error! Unable to configure Docker images."
+   echo -e "\e[91mError! Unable to configure Docker images.\e[0m"
    exit 1
 fi
 
-# Check if Docker containers deployed
-echo 'Checking installation...'
+# Check if Docker containers deployed are running
+echo -e "\e[35mChecking installation...\e[0m"
 if docker inspect -f '{{.State.Running}}' 'frxpi-APACHE' | grep -q 'true' && docker inspect -f '{{.State.Running}}' 'frxpi-MYSQL' | grep -q 'true' ; then
-   echo "-----"
+   echo ""
    # Get Raspberry Pi local network IP address
    rpi_ip="$(ip addr | grep 'wlan0' | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | head -1)"
-   msg="Visit http://${rpi_ip}/ on your local network to access the FruxePi Dashboard."
-   echo "Installation complete!"
-   echo $msg
+   msg="Visit \e[1mhttp://${rpi_ip}/\e[0m on your local network to access the FruxePi Dashboard."
+   echo -e "\e[45mInstallation complete!\e[0m"
+   echo -e $msg
+   echo ""
 else
-   echo "Error! Installation process encountered errors and is incomplete."
+   echo -e "\e[91mError! Installation process encountered errors and is incomplete.\e[0m"
    exit 1
 fi
