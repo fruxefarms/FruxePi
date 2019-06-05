42566a5edac516ce92727295840488e35b3eef8d
Jay Yeo <jay@fruxe.co>
2019-06-05 22:28:39 +0200

    Added robots.txt file to stop bot indexing

--- app/application/views/auth/login.php
+++ app/application/views/auth/login.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
+    <meta name="robots" content="noindex,nofollow" />
 
     <title>FruxePi</title>
 
